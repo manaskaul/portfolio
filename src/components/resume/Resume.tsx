b8e79fd5fd24a772da3ee773f673f67769ff067e
kaul <kaulmanascocid@gmail.com>
2024-04-07 13:50:21 +0530

    resume

--- src/components/resume/Resume.tsx
+++ src/components/resume/Resume.tsx
@@ -1,150 +1,22 @@
-import "./Resume.css";
+import { Resume, Section, SubSection } from "../../interfaces/resume";
 import resumeJson from "../../resume.json";
-import { Contact, Resume, Section, SubSection } from "../../interfaces/resume";
-
-// export function LeftNav({ resume }) {
-//   const leftNavLinks = ["Manas Kaul"];
-//   resume.sections.forEach((resumeSection) => {
-//     leftNavLinks.push(resumeSection.title);
-//   });
-
-//   const leftNavLinksList = leftNavLinks.map((linkText, idx) => {
-//     const scrollPos = `#${linkText}`;
-
-//     return (
-//       <a href={scrollPos} key={idx}>
-//         {linkText}
-//       </a>
-//     );
-//   });
-
-//   return (
-//     <div className="left-nav">
-//       <ul
-//         style={{
-//           marginTop: "40px",
-//           display: "flex",
-//           flexDirection: "column",
-//           padding: "0",
-//         }}
-//       >
-//         {leftNavLinksList}
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export function RightContent({ resume: any }) {
-//   return (
-//     <div className="right-content">
-//       <Header resume={resume} />
-//       <Section resume={resume} />
-//     </div>
-//   );
-// }
-
-// export function Header({ resume }) {
-//   return (
-//     <div id="Manas Kaul">
-//       <p
-//         style={{
-//           fontSize: "25px",
-//           fontWeight: "bold",
-//         }}
-//       >
-//         {resume.designation}
-//       </p>
-//       <p>{resume.description}</p>
-//     </div>
-//   );
-// }
-
-// export function Section({ resume }) {
-//   let allResume = resume.sections.map((resumeSection, idx) => {
-//     let subSections = resumeSection.subsection.map((subSec, idx) => {
-//       let subSectionDesc = subSec.description.map((desc, idx) => {
-//         return (
-//           <div key={idx}>
-//             <li>{desc}</li>
-//           </div>
-//         );
-//       });
-
-//       return (
-//         <div
-//           style={{
-//             marginLeft: "25px",
-//           }}
-//           key={idx}
-//         >
-//           <span>
-//             <span
-//               style={{
-//                 fontSize: "16px",
-//                 fontWeight: "bold",
-//               }}
-//             >
-//               {subSec.heading}
-//             </span>{" "}
-//             | <span>{subSec.subHeading1}</span>
-//           </span>
-//           <div>{subSec.subHeading2}</div>
-//           <ul>{subSectionDesc}</ul>
-//         </div>
-//       );
-//     });
-
-//     return (
-//       <div key={idx} id={resumeSection.title}>
-//         <div
-//           style={{
-//             fontSize: "20px",
-//             fontWeight: "bold",
-//             marginBottom: "12px",
-//           }}
-//         >
-//           {resumeSection.title}
-//         </div>
-//         {subSections}
-//         <br />
-//       </div>
-//     );
-//   });
-//   return <>{allResume}</>;
-// }
+import "./Resume.css";
 
 export default function Resume() {
   const resume: Resume = JSON.parse(JSON.stringify(resumeJson));
-  console.log({ resume });
-
-  const contactList = resume.contact.map((c: Contact, i: number) => {
-    return (
-      <>
-        <li key={i}>
-          <a href={c.link} target="_blank" rel="noopener noreferrer">
-            {c.text}
-          </a>
-        </li>
-      </>
-    );
-  });
 
   const sections = resume.sections.map((section: Section, i: number) => {
     const subSections = section.subsection.map(
       (subSection: SubSection, i: number) => {
         const subSectionDesc = subSection.description.map(
           (desc: string, i: number) => {
-            return (
-              <>
-                <li key={i}>{desc}</li>
-              </>
-            );
+            return <li key={i}>{desc}</li>;
           }
         );
 
         return (
           <div className="sub-heading" key={i}>
-            <h4>{subSection.heading}</h4>
+            <h3>{subSection.heading}</h3>
             <p>{subSection.subHeading1}</p>
             <p>{subSection.subHeading2}</p>
             <ul>{subSectionDesc}</ul>
@@ -155,7 +27,7 @@ export default function Resume() {
 
     return (
       <div className="sections" key={i}>
-        <h3>{section.title}</h3>
+        <h2>{section.title}</h2>
         <>{subSections}</>
       </div>
     );
@@ -164,38 +36,50 @@ export default function Resume() {
   return (
     <div className="resume">
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis vel
-        quas alias expedita! Voluptates nihil corporis perferendis doloremque
-        qui expedita facilis, sit totam ex reprehenderit quos, amet veritatis
-        sed corrupti! Voluptatibus ad eligendi enim quod repudiandae doloremque,
-        beatae non reprehenderit minus distinctio hic nobis voluptatum
-        temporibus? Temporibus voluptates similique ad!
+        I'm a Full-Stack Engineer at XPO, where I build dynamic web applications
+        using Angular and TypeScript and robust and reliable backend systems
+        using Java. I have a Bachelor of Technology degree in Computer Science
+        from Dhirubhai Ambani Institute of Information and Communication
+        Technology (DAIICT), one of the top engineering colleges in India.
       </p>
       <div className="summary">
         <h3>Summary</h3>
         <div className="sub-heading">
           <h4>{resume.name}</h4>
           <p>{resume.description}</p>
-          <ul>{contactList}</ul>
+          <ul>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/manas-kaul/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                linkedin.com/in/manas-kaul/
+              </a>
+            </li>
+            <li>
+              <a
+                href="mailto:manas.kaul2@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                manas.kaul2@gmail.com
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://maps.app.goo.gl/v5uvRSUg74GRqNcC8"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Pune, Maharashtra
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
 
-      {/* <div className="sections">
-        <h3>Heading</h3>
-        <div className="sub-heading">
-          <h4>Sub-Heading</h4>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe odit
-            repellendus maiores dicta illo impedit ratione quis, veniam
-            similique maxime?
-          </p>
-        </div>
-      </div> */}
-
       <>{sections}</>
-
-      {/* <LeftNav resume={resume} />
-      <RightContent resume={resume} /> */}
     </div>
   );
 }
