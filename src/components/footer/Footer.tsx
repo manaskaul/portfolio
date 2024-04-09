18df46f50c2bc4f50cd854f4ce42851bc8ad9874
kaul <kaulmanascocid@gmail.com>
2024-04-09 23:26:23 +0530

    styling changes

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,39 +6,39 @@ export default function Footer() {
       <div className="footer-link">
         <div className="title">Email</div>
         <div className="sub-title">
-          <img src="./assets/icons/email.svg" alt="email" />
           <a
             href="mailto:manas.kaul2@gmail.com"
             target="_blank"
             rel="noopener noreferrer"
           >
-            manas.kaul2@gmail.com
+            <img src="./assets/icons/email.svg" alt="email" />
+            manas.kaul2
           </a>
         </div>
       </div>
       <div className="footer-link">
         <div className="title">GitHub</div>
         <div className="sub-title">
-          <img src="./assets/icons/github.svg" alt="github" />
           <a
             href="https://github.com/manaskaul"
             target="_blank"
             rel="noopener noreferrer"
           >
-            https://github.com/manaskaul
+            <img src="./assets/icons/github.svg" alt="github" />
+            manaskaul
           </a>
         </div>
       </div>
       <div className="footer-link">
         <div className="title">LinkedIn</div>
         <div className="sub-title">
-          <img src="./assets/icons/linkedin.svg" alt="linkedIn" />
           <a
-            href="https://www.linkedin.com/in/kaul-manas/"
+            href="https://www.linkedin.com/in/manas-kaul/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            https://www.linkedin.com/in/kaul-manas/
+            <img src="./assets/icons/linkedin.svg" alt="linkedIn" />
+            manas-kaul
           </a>
         </div>
       </div>
