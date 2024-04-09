1b582d9c65ea110fb0ca0ea5cf9e75a81bf9ed8f
kaul <kaulmanascocid@gmail.com>
2024-04-09 22:39:39 +0530

    changes to resume

--- src/components/personal/Personal.tsx
+++ src/components/personal/Personal.tsx
@@ -1,7 +1,15 @@
 import "./Personal.css";
 
 function Personal() {
-  return <div>Personal works!</div>;
+  return (
+    <>
+      <div className="heading">
+        <h1>Personal</h1>
+        <div className="">What am I doing when I'm not working?</div>
+        <div className="divider"></div>
+      </div>
+    </>
+  );
 }
 
 export default Personal;
--- src/components/resume/Resume.tsx
+++ src/components/resume/Resume.tsx
@@ -34,52 +34,69 @@ export default function Resume() {
   });
 
   return (
-    <div className="resume">
-      <p>
-        I'm a Full-Stack Engineer at XPO, where I build dynamic web applications
-        using Angular and TypeScript and robust and reliable backend systems
-        using Java. I have a Bachelor of Technology degree in Computer Science
-        from Dhirubhai Ambani Institute of Information and Communication
-        Technology (DAIICT), one of the top engineering colleges in India.
-      </p>
-      <div className="summary">
-        <h3>Summary</h3>
-        <div className="sub-heading">
-          <h4>{resume.name}</h4>
-          <p>{resume.description}</p>
-          <ul>
-            <li>
-              <a
-                href="https://www.linkedin.com/in/manas-kaul/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                linkedin.com/in/manas-kaul/
-              </a>
-            </li>
-            <li>
-              <a
-                href="mailto:manas.kaul2@gmail.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                manas.kaul2@gmail.com
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://maps.app.goo.gl/v5uvRSUg74GRqNcC8"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Pune, Maharashtra
-              </a>
-            </li>
-          </ul>
+    <>
+      <div className="heading">
+        <h1>Resume</h1>
+        <div className="">
+          Full resume can be found{" "}
+          <a
+            href="./assets/manas_kaul-resume.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            here.
+          </a>
         </div>
+        <div className="divider"></div>
       </div>
+      <div className="resume">
+        <p>
+          I'm a Full-Stack Engineer at XPO, where I build dynamic web
+          applications using Angular and TypeScript and robust and reliable
+          backend systems using Java. I have a Bachelor of Technology degree in
+          Computer Science from Dhirubhai Ambani Institute of Information and
+          Communication Technology (DAIICT), one of the top engineering colleges
+          in India.
+        </p>
+        <div className="summary">
+          <h3>Summary</h3>
+          <div className="sub-heading">
+            <h4>{resume.name}</h4>
+            <p>{resume.description}</p>
+            {/* <ul>
+              <li>
+                <a
+                  href="https://www.linkedin.com/in/manas-kaul/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  linkedin.com/in/manas-kaul/
+                </a>
+              </li>
+              <li>
+                <a
+                  href="mailto:manas.kaul2@gmail.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  manas.kaul2@gmail.com
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://maps.app.goo.gl/v5uvRSUg74GRqNcC8"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Pune, Maharashtra
+                </a>
+              </li>
+            </ul> */}
+          </div>
+        </div>
 
-      <>{sections}</>
-    </div>
+        <>{sections}</>
+      </div>
+    </>
   );
 }
